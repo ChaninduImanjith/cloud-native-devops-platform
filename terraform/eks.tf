d97ca8694cbeb6cc5236a8f12946b28742441e56
chaninduimanjith <chanindu.imanjith@gmail.com>
2026-06-24 09:42:20 +0530

    feat(eks): enable VPC CNI prefix delegation and increase max pods capacity
    - Add vpc-cni cluster addon with ENABLE_PREFIX_DELEGATION=true and WARM_PREFIX_TARGET=1
    - Add pre-bootstrap user data to disable default max pods limit
    - Set kubelet extra args to allow up to 110 pods per node
    - Add max-pods-calculator.sh helper script
    - Revert instance type back to t3.micro (cost optimization)

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -9,6 +9,19 @@ module "eks" {
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
+  cluster_addons = {
+    vpc-cni = {
+      most_recent    = true
+      before_compute = true
+      configuration_values = jsonencode({
+        env = {
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
+    }
+  }
+
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
   }
@@ -16,11 +29,17 @@ module "eks" {
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
 
       min_size     = 2
       max_size     = 4
       desired_size = 3
+
+      pre_bootstrap_user_data = <<-EOT
+        export USE_MAX_PODS=false
+      EOT
+
+      bootstrap_extra_args = "--kubelet-extra-args '--max-pods=110'"
     }
   }
 }
